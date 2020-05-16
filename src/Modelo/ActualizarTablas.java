5fa86156b85236c32051265e50657223818c7c24
LettyC23 <44488698+LettyC23@users.noreply.github.com>
2020-05-16 15:53:24 -0500

    Clase ActualizarTabla con método para actualizar los registros en la tabla de la ventana

---
+++ src/Modelo/ActualizarTablas.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.sql.SQLException;
+import javax.swing.JButton;
+import javax.swing.JTable;
+
+/**
+ *
+ * @author tmx
+ */
+public class ActualizarTablas {
+    public void actualizarTabla(JTable tabla) {
+			String controlador = "oracle.jdbc.OracleDriver";
+			String url = "jdbc:oracle:thin:@localhost:1521:XE";
+			String consulta = "SELECT * FROM Clientes ";
+			ResultSetTableModel modelDatos = null;
+                        
+			try {
+				modelDatos = new ResultSetTableModel(controlador, url, "System", "EStrell25", consulta);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			tabla.setModel(modelDatos);
+		}
+}
