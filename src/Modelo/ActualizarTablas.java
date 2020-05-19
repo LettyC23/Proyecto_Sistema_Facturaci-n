e1b6b56fa7510f533bc1b135e5fb6693e7524d58
LettyC23 <44488698+LettyC23@users.noreply.github.com>
2020-05-18 22:08:45 -0500

    Método para filtrar registros

--- src/Modelo/ActualizarTablas.java
+++ src/Modelo/ActualizarTablas.java
@@ -32,4 +32,24 @@ public class ActualizarTablas {
 			
 			tabla.setModel(modelDatos);
 		}
+    
+    public void actualizarTablaFiltro(String consulta, JTable tabla){
+                    String controlador = "oracle.jdbc.OracleDriver";
+			String url = "jdbc:oracle:thin:@localhost:1521:XE";
+                       // String consulta = "SELECT * FROM Clientes WHERE Nombre LIKE '%"+nombre+"'%";
+                        ResultSetTableModel modelDatos = null;
+                        
+                        try {
+				modelDatos = new ResultSetTableModel(controlador, url, "System", "EStrell25", consulta);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+                        
+                      tabla.setModel(modelDatos); 
+                     
+                }
 }
